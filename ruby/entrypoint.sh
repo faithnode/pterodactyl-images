7bd95abd916e49b0ff631ac7951e04ba65b3bf24
zloy-roma <roman21113007@gmail.com>
2022-09-05 12:57:56 +0300

    ruby added

---
+++ ruby/entrypoint.sh
@@ -0,0 +1,13 @@
+cd /home/container || exit 1
+
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+ruby -v
+printf "\n"
+
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+printf "$PARSED"
+
+eval ${PARSED}
